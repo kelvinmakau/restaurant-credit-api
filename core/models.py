f21956dff507b4a66831f23d5024b6493303471f
Kelvin Makau <kelvinmakau76@gmail.com>
2025-08-13 12:33:25 +0300

    Updated Payment models to enable partial or multiple payments

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
 # Models I will be using for my restaurant credit system api app
@@ -9,13 +10,13 @@ class User(AbstractUser):
     full_name = models.CharField(max_length=100, blank=True)  # Full name of the user
     phone_number = models.CharField(max_length=15, unique=True, blank=True)  # User phone number
     ROLE_CHOICES = ( # Defining my user roles
-        ('ADMIN', 'admin'),
-        ('WAITER', 'waiter')
+        ('ADMIN', 'ADMIN'), # Admin role
+        ('WAITER', 'WAITER'), # Waiter role
     )
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES) # User role field
-
-    date_joined = models.DateTimeField(auto_now_add=True)  # Date when the user joined
+    # date_joined comes from AbstractUser
+    # is_active also comes from AbstractUser
 
 
     def __str__(self):
@@ -23,17 +24,21 @@ class User(AbstractUser):
     
 # Customer
 class Customer(models.Model):
-    name = models.CharField(max_length=100) # Customer name
+    full_name = models.CharField(max_length=100, blank=True) # Customer name
     phone_number = models.CharField(max_length=15, unique=True) # Customer phone number
-    email = models.EmailField(unique=True) # Customer email    
+    email = models.EmailField(blank=True, null=True) # Customer email 
+    date_created = models.DateField(auto_now_add=True) # Date when the customer was created
+
+    def __str__(self):
+        return self.full_name   
 # Meal
 class Meal(models.Model):
     name = models.CharField(max_length=100) # Meal name
-    description =  models.TextField() # Meal description
+    description =  models.TextField(blank=True) # Meal description, optional
     price = models.DecimalField(max_digits=10, decimal_places=2) # Meal price
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.price}" # Meal name and price
     
 # Order
 class Order(models.Model):
@@ -42,9 +47,11 @@ class Order(models.Model):
     quantity = models.PositiveIntegerField(default=1) # Quantity of the meal ordered
     total_price = models.DecimalField(max_digits=10, decimal_places=2) # Total price for the order
     created_at = models.DateTimeField(auto_now_add=True) # Order creation time
+    is_paid = models.BooleanField(default=False) # Payment status of the order
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_orders') # User who created the order
 
     def __str__(self):
-        return f"Order {self.id} by {self.customer.user.username}"
+        return f"Order {self.id} by {self.customer.full_name}"
     
 # Payment
 class Payment(models.Model):
@@ -53,7 +60,7 @@ class Payment(models.Model):
         ('MPESA', 'M-Pesa')
         )
 
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment') # Order associated with the payment
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='payments') # Order associated with the payment, customer can pay partially
     amount = models.DecimalField(max_digits=10, decimal_places=2) # Amount paid
     payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES) # Payment method used
     payment_date = models.DateTimeField(auto_now_add=True) # Payment date
